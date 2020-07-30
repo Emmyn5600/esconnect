2ab00d96738df04d6bfff85ce7225ed3b6dfca39
emmyn5600 <emmy66418@gmail.com>
2020-07-30 14:04:11 +0200

    remove cors

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import AppError from "./utils/appError";
 import customerRouter from "./routes/customerRoutes";
 import productRouter from "./routes/productRoutes";
 import orderRouter from "./routes/orderRoutes";
-import cors from 'cors';
+// import cors from 'cors';
 
 const app = express();
 
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1/customers", customerRouter);
